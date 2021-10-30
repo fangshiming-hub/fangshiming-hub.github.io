63ca28703014c841b6d2c4a10d12e772ffe4cc1f
fangshiming <fsm510@outlook.com>
2021-10-30 08:35:18 +0800

    message

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,96 @@
+module.exports = {
+    title: "Hello VuePress",
+    description: "Just plating around",
+    base: "/fangshiming-hub/",
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        sidebarDepth: 2,
+        lastUpdated: 'Lase Updated',
+        // 顶部导航
+        nav: [
+            {
+                text: "首页",
+                link: "/"
+            },
+            {
+                text: "框架",
+                link: "/frame/"
+            },
+
+            {
+                text: "算法",
+                link: "/algorithm/"
+            },
+            {
+                text: "javaScript",
+                link: "/javaScript/"
+            },
+            {
+                text: "博客导航",
+                link: "/blog/"
+            },
+            {
+                text: "GitHub",
+                link: "/github/"
+            },
+        ],
+        // 侧边导航
+        sidebar:{
+            "/frame/": [
+                '/frame/',
+                {
+                    title: 'vue',
+                    children: [
+                        '/frame/vue/',
+                        '/frame/vue/model',
+
+                    ]
+                },
+                {
+                    title: 'angular',
+                    children: [
+                        '/frame/angular/'
+                    ]
+                },
+                {
+                    title: 'react',
+                    children: [
+                        '/frame/react/'
+                    ]
+                }
+            ],
+            "/javaScript/": [
+                '/javaScript/',
+                {
+                    title: 'javaScript',
+                    children: [
+                        '/javaScript/test01/'
+                    ]
+                },
+                {
+                    title: 'javaScript',
+                    children: [
+                        '/javaScript/test02/'
+                    ]
+                },
+                {
+                    title: 'javaScript',
+                    children: [
+                        '/javaScript/test03/'
+                    ]
+                }
+            ],
+            "/algorithm/": [],
+            "/font/": ['test01/','test02/','test03/'],
+            "/foo/": [
+                '',     /* /foo/ */
+                'one',  /* /foo/one.html */
+                'two'   /* /foo/two.html */
+            ],
+            "/": ['']
+        }
+
+    }
+}
---
+++ docs/javaScript/test02/index.md
@@ -0,0 +1,2 @@
+test02![img](file:///C:\Users\hp\AppData\Local\Temp\SGPicFaceTpBq\6076\00272B09.png)
+
