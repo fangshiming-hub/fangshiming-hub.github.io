55e46d33e84d6b484589faa82c84480c8e57fc5d
fangshiming <fsm510@outlook.com>
2021-11-04 23:12:20 +0800

    message

--- docs/.vuepress/components/RegCollect.vue
+++ docs/.vuepress/components/RegCollect.vue
@@ -2,23 +2,46 @@
     <div class="reg_wrap">
         <div>
             <h3>测试一下</h3>
-            <el-checkbox v-model="isGlobal">全局搜索</el-checkbox>
-            <el-checkbox v-model="isIgnoreLower">忽略大小写</el-checkbox>
             <p>正则表达式</p>
-            <el-input 
-                v-model="regParren"
-                placeholder="请输入正则表达式"
-                clearable
-            ></el-input>
+            <div class="regs">
+                <el-input 
+                    v-model="regParren"
+                    placeholder="请输入正则表达式"
+                    clearable
+                ></el-input>
+                <el-checkbox v-model="isGlobal">全局搜索</el-checkbox>
+                <el-checkbox v-model="isIgnoreLower">忽略大小写</el-checkbox>
+            </div>
             <p>测试的值</p>
             <el-input 
                 v-model="testVal"
                 placeholder="请输入要测试的值"
+                type="textarea"
                 clearable
             ></el-input>
             <p></p>
-            <el-button type="primary" size="mini" @click="testReg">测试</el-button>
-            <p>测试结果:{{ result }}</p>
+            <el-button type="primary" size="mini" @click="matching('reg')">正则匹配</el-button>
+            <el-button type="primary" size="mini" @click="matching('str')">字符串匹配</el-button>
+            <p>字符串匹配结果:{{ isMatching }}</p>
+            <span >正则匹配结果:</span>
+            <p></p>
+            <el-input type="textarea" v-model="result"></el-input>
+            <p>替换文本</p>
+            <div>
+                <el-input
+                    v-model="replaceStr"
+                    placeholder="请输入替换文本"
+                >
+                    <template slot="append">
+                        <el-button type="primary" @click="replaceString">替换</el-button>
+                    </template>
+                </el-input>
+                <p></p>
+            </div>
+            <div>
+                <p>替换结果</p>
+                <el-input type="textarea" v-model="replaceResult"></el-input>
+            </div>
         </div>
         <el-divider>常见正则</el-divider>
         <div 
@@ -61,9 +84,13 @@ export default{
             ],
             regParren: '',
             testVal: '',
-            result: '',
+            isMatching: '',
             isGlobal: false,
-            isIgnoreLower: true
+            isIgnoreLower: true,
+            result: '',
+            curRegexp: '',
+            replaceStr:'',
+            replaceResult: ''
         }
     },
     methods: {
@@ -85,7 +112,7 @@ export default{
             document.body.removeChild(input);
         },
         // 测试正则
-        testReg() {
+        matching(rule) {
             if(this.regParren && this.testVal) {
                 let reg = this.regParren;
                 if(this.isIgnoreLower) {
@@ -96,9 +123,21 @@ export default{
                 }
                 try {
                     reg = eval(reg);
-                    const res = reg.test(this.testVal);
-                    this.result = res ? "匹配" : '不匹配';
+                    this.curRegexp = reg;
+                    if(rule === 'reg') {
+                        const res = reg.test(this.testVal);
+                        this.isMatching = res ? "匹配" : '不匹配';
+                    } else if(rule === 'str') {
+                        const res = this.testVal.match(reg);
+                        if(this.isGlobal) {
+                            this.result = `共找到${res.length}个匹配\n${res.join(' ')}`;
+                        } else {
+                            this.result = `匹配位置:${res['index']}\n匹配结果:${res[0]}`;
+                        }
+                        console.log(res);
+                    }
                 } catch (error) {
+                    this.curRegexp = '';
                     this.$message({
                         type:'error',
                         message: '请输入正确的正则表达式'
@@ -110,12 +149,30 @@ export default{
                     message: '请输入正则表达式和测试的值!'
                 })
             }
+        },
+        // 替换
+        replaceString() {
+            if(this.curRegexp && this.testVal && this.replaceStr){
+                this.replaceResult = this.testVal.replace(this.curRegexp, this.replaceStr)
+                console.log(this.replaceResult, 99);
+            } else {
+                this.replaceResult = '';
+            }
         }
+
     }
 
 }
 </script>
 <style scoped>
+.regs{
+    display: flex;
+    align-items: center;
+}
+.regs .el-input{
+    padding-right: 20px;
+   flex: 1;
+}
 .reg_item{
     display: flex;
     margin-bottom: 16px;
